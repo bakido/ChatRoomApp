8d6ec0c2cdfe1da4685b68a810394234275be639
ntsoba01 <ntsoba01@CPT-AA009659>
2017-10-01 14:26:03 +0200

    Simple chat application

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatApp.Hubs
+{
+   
+    public class ChatHub:Hub
+    {
+        public void Send(string name,string message)
+        {
+            Clients.All.addnewMessageToPage(name, message);
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+            
+        }
+    }
+}
